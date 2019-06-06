f2399b6f0b1d448934b900a6f7549b3a352973e7
Bobo-peng <396400791@qq.com>
2019-06-06 23:06:56 +0800

    15

---
+++ 剑指offer(version2)/Ocoding/011_MinInRotatedArry/011_MinInRotatedArry.cpp
@@ -0,0 +1,44 @@
+﻿// 011_MinInRotatedArry.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//==================================================================
+// author:pb
+// date:2019-06-06
+//==================================================================
+
+#include "pch.h"
+#include <iostream>
+#include<vector>
+int MinNumInRotateArry(std::vector<int> &arr)
+{
+	int start = 0;
+	int end = arr.size()-1;
+	int mid = 0;
+	while (arr[start] >= arr[end])
+	{
+		if (end - start == 1)
+		{
+			mid = end;
+			break;
+		}
+		mid = (end - start) / 2 + start;
+		if (arr[mid] >= arr[start])
+			start = mid;
+		else if (arr[mid] <= arr[end])
+			end = mid;
+	}
+	return arr[mid];
+}
+void Test()
+{
+	std::vector<int> arr{3,4,5,1,2};
+	std::vector<int> arr1{1,2,3,4,5};
+	std::vector<int> arr2{ 5,4,3,2,1 };
+	int ret =MinNumInRotateArry(arr);
+	 ret = MinNumInRotateArry(arr1);
+	 ret = MinNumInRotateArry(arr2);
+}
+int main()
+{
+	Test();
+	return 0;
+}
+
---
+++ 剑指offer(version2)/Ocoding/015_BinaryBit/015_BinaryBit.cpp
@@ -0,0 +1,31 @@
+﻿// 015_BinaryBit.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//==================================================================
+// author:pb
+// date:2019-06-06
+//==================================================================
+
+#include "pch.h"
+#include <iostream>
+int BinaryBit(int n)
+{
+	int cnt = 0;
+	unsigned int tmp = 1;
+	while (tmp)
+	{
+		if (n & tmp)
+			++cnt;
+		tmp <<= 1;
+	}
+	return cnt;
+}
+void Test()
+{
+	int ret = BinaryBit(1);
+	ret = BinaryBit(3);
+	ret = BinaryBit(0xFF88);
+	ret = BinaryBit(0x9999);
+}
+int main()
+{
+	Test();
+}
