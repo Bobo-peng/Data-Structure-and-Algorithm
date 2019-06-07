02a0507dd86301a8d9fb16cd2fa0af4d284be32d
Bobo-peng <396400791@qq.com>
2019-06-08 02:31:58 +0800

    17

---
+++ 剑指offer(version2)/Ocoding/016_Pow/016_Pow.cpp
@@ -0,0 +1,49 @@
+﻿// 016_Pow.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//==================================================================
+// author:pb
+// date:2019-06-06
+//==================================================================
+
+#include "pch.h"
+#include <iostream>
+//浮点数的比较
+bool equal(double x, double y)
+{
+	if ((x - y > -0.0000001) && (x - y < 0.0000001))
+		return true;
+	else
+		return false;
+}
+double _Opow(double n, unsigned int e)
+{
+	if (e == 0)
+		return 1;
+	if (e == 1)
+		return n;
+	int ret = _Opow(n, e >> 1);//除以2
+	ret *= ret;
+	if (e & 1 == 1)//判断奇偶
+		ret *= n;
+	return ret;
+}
+double Opow(double n, int e)
+{
+	if (equal(n, 0.0) && e < 0)//排除分母为0的情况
+		return 0.0;
+	int _e = e > 0 ? e : -e;
+	double ret = _Opow(n, _e);
+	if (e < 0)
+		ret = 1.0 / ret;
+	return ret;
+}
+int main()
+{
+	double a =pow(2.0,-1);
+	a = pow(3, -2);
+	double b = Opow(2.0,-1);
+	double c = Opow(2.0, 0);
+	double d = Opow(0, 0);
+	double e = Opow(3, -2);
+    
+}
+
---
+++ 剑指offer(version2)/Ocoding/017_Print1toN/017_Print1toN.cpp
@@ -0,0 +1,51 @@
+﻿// 017_Print1toN.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//==================================================================
+// author:pb
+// date:2019-06-06
+//==================================================================
+
+#include "pch.h"
+#include <iostream>
+void Print1toN(int n)
+{
+	//std::unique_ptr<char> num(new char[n]());
+	int *num = new int[n]();
+	int len = 0;
+	bool overflow = false;
+	while (!overflow)
+	{
+		int carry = 0;
+		for (int i = 0; i < n; ++i)//把各位放在num[0],打印的时候反着打印
+		{
+			int tmp = num[i] + carry;
+			if (i == 0)	tmp++;//只在个位加1
+			if (tmp >= 10)//有进位
+			{
+				tmp %= 10;
+				carry = 1;
+				num[i] = tmp;
+				
+				if (i == n - 1) overflow = true;	
+			}
+			else
+			{
+				num[i] = tmp;
+				len = len > i ? len : i;//求总共有多少位数
+				break;
+			}
+		}
+		if (overflow) break;//最高位有进位，说明结束了，直接跳出大循环
+		for (int i = len; i >= 0; --i)
+		{
+			std::cout << num[i];
+		}
+		std::cout << std::endl;
+	}
+	delete[] num;
+}
+int main()
+{
+	Print1toN(1);
+	Print1toN(4);
+	getchar();
+}
