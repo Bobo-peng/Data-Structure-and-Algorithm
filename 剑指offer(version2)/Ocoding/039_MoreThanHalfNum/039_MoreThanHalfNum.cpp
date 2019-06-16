7ba8b6e0f05b860b40a05e98402eb1aea45fc50b
Bobo-peng <396400791@qq.com>
2019-06-16 10:04:18 +0800

    39

--- 剑指offer(version2)/Ocoding/039_MoreThanHalfNum/039_MoreThanHalfNum.cpp
+++ 剑指offer(version2)/Ocoding/039_MoreThanHalfNum/039_MoreThanHalfNum.cpp
@@ -1,6 +1,8 @@
 ﻿// 039_MoreThanHalfNum.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+//==================================================================
+// author:pb
+// date:2019-06-016
+//==================================================================
 #include "pch.h"
 #include <iostream>
 #include<vector>
@@ -22,6 +24,7 @@ int partion(std::vector<int> &arr,int start, int end)
 }
 int MoreThanHalfNumber(std::vector<int> &arr)
 {
+	if (arr.size() < 1) return 0;
 	int start = 0;
 	int end = arr.size()-1;
 	int mid = end >> 1;
@@ -41,12 +44,34 @@ int MoreThanHalfNumber(std::vector<int> &arr)
 	}
 	return arr[p];
 }
+int MoreThanHalfNumber2(std::vector<int> &arr)
+{
+	if (arr.size() < 1) return 0;
+	int len = arr.size();
+	int num = arr[0];//保存数组的值
+	int times = 1;//保存出现的次数
+	for (int i = 1; i < len; ++i)
+	{
+		if (times == 0)
+		{
+			num = arr[i];
+			times = 1;
+		}
+		else if (arr[i] == num)
+			times++;
+		else
+			times--;
+	}
+	return num;
+}
 void Test()
 {
 	std::vector<int> arr{1,2,3,2,2,2,5,4,2};
 	std::vector<int> arr1{ 1,2,3,2,3,3,5,3,3 };
 	int ret = MoreThanHalfNumber(arr);
 	 ret = MoreThanHalfNumber(arr1);
+	 ret = MoreThanHalfNumber2(arr);
+	 ret = MoreThanHalfNumber2(arr1);
 }
 int main()
 {
